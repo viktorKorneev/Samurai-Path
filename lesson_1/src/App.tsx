b2545e4036f39eb3c0426ea9806426c60561c284
viktorKorneev <korneevviktor90@yandex.ru>
2026-07-29 11:26:02 +0300

    first commit

---
+++ lesson_1/src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            {/*const tasks = [*/}
+            {/*{ id: 1, title: "Купить продукты на неделю", isDone: false },*/}
+            {/*{ id: 2, title: "Полить цветы", isDone: true },*/}
+            {/*{ id: 3, title: "Сходить на тренировку", isDone: false },*/}
+            {/*]*/}
+
+            <h1>Список дел</h1>
+            <ul>
+                <li>
+                    <div>Купить продукты на неделю</div>
+                    <input type={"checkbox"}/>
+                </li>
+                <li>
+                    <div>Полить цветы</div>
+                    <input type={"checkbox"}/>
+                </li>
+                <li>
+                    <div>Сходить на тренировку</div>
+                    <input type={"checkbox"}/>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default App
---
+++ lesson_2/map.js
@@ -0,0 +1,3 @@
+const numbers = [1, 5, 10, 15, 20]
+const newNumbers = numbers.map((number) => {return  number + 10})
+console.log(newNumbers) // [11, 15, 20, 25, 30] ⬅️ Итоговый результат
