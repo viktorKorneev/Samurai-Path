abbb58a885e46e2314c6bee40d463f1bfc74ba4b
viktorKorneev <korneevviktor90@yandex.ru>
2026-07-29 11:44:41 +0300

    step-4

--- lesson_2/map.js
+++ lesson_2/map.js
@@ -38,3 +38,26 @@ console.log(productsWithFinalPrice)
 //   { name: 'Mouse', price: 50, finalPrice: 42.5 },
 //   { name: 'Keyboard', price: 80, finalPrice: 68 }
 // ]
+
+const employees = [
+    { name: "Alice", salary: 50000, experience: 3 },
+    { name: "Bob", salary: 60000, experience: 5 },
+    { name: "Charlie", salary: 45000, experience: 2 },
+]
+
+const employeesWithTotal = employees.map((employee) => {
+    return {
+        name: employee.name,
+        salary: employee.salary,
+        experience: employee.experience,
+        totalSalary: employee.salary * 0.05 * employee.experience + employee.salary,
+    }
+})
+
+console.log(employeesWithTotal)
+// ⬇️ Итоговый результат
+// [
+//   { name: 'Alice', salary: 50000, experience: 3, totalSalary: 57500 },
+//   { name: 'Bob', salary: 60000, experience: 5, totalSalary: 75000 },
+//   { name: 'Charlie', salary: 45000, experience: 2, totalSalary: 49500 }
+// ]
