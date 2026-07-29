55067885b1124538a7b2a41a812c9d76ba625c8c
viktorKorneev <korneevviktor90@yandex.ru>
2026-07-29 14:07:16 +0300

    step-2

---
+++ lesson_3/music-player/player.js
@@ -0,0 +1,11 @@
+ export function play() {
+    console.log("▶️ Воспроизведение началось")
+}
+
+export function pause() {
+    console.log("⏸️ Воспроизведение приостановлено")
+}
+
+export function stop() {
+    console.log("⏹️ Воспроизведение остановлено")
+}
